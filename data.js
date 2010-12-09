142981b3c5984ca27c0b88b7cc2ecc809ad0952a
bjornd <BjornD@bjornd-mac.local>
2010-12-09 15:03:13 +0500

    splitting images to rows

---
+++ data.js
@@ -0,0 +1,58 @@
+var galleryData = {
+    title: "Paris May 2010",
+    photos: [{
+        title: "Eiffel Tower",
+        published: "01.05.2010",
+        src: "tmp/1.jpg",
+        width: 1024,
+        height: 768,
+        thumb_src: "tmp/1_thumb.jpg",
+        thumb_width: 266,
+        thumb_height: 200
+    },{
+        title: "View from Eiffel Tower",
+        published: "02.05.2010",
+        src: "tmp/2.jpg",
+        width: 304,
+        height: 480,
+        thumb_src: "tmp/2_thumb.jpg",
+        thumb_width: 150,
+        thumb_height: 236
+    },{
+        title: "Arc de Triomphe",
+        published: "03.05.2010",
+        src: "tmp/3.jpg",
+        width: 1024,
+        height: 768,
+        thumb_src: "tmp/3_thumb.jpg",
+        thumb_width: 266,
+        thumb_height: 200
+    },{
+        title: "Senna River Bridge",
+        published: "04.05.2010",
+        src: "tmp/4.jpg",
+        width: 1111,
+        height: 456,
+        thumb_src: "tmp/4_thumb.jpg",
+        thumb_width: 487,
+        thumb_height: 200
+    },{
+        title: "On the Roof",
+        published: "05.05.2010",
+        src: "tmp/5.jpg",
+        width: 600,
+        height: 450,
+        thumb_src: "tmp/5_thumb.jpg",
+        thumb_width: 266,
+        thumb_height: 200
+    },{
+        title: "Eifel Tower at Night",
+        published: "06.05.2010",
+        src: "tmp/6.jpg",
+        width: 800,
+        height: 546,
+        thumb_src: "tmp/6_thumb.jpg",
+        thumb_width: 293,
+        thumb_height: 200
+    }]
+};
---
+++ jquery.gallery.js
@@ -0,0 +1,91 @@
+$(function(){
+   $('.b-gallery').gallery({
+        maxWidth: 266,
+        minWidth: 200,
+        maxHeight: 200,
+        minHeight: 150,
+        interval: 10,
+        data: galleryData
+    });
+});
+
+(function( $ ){
+    $.fn.gallery = function(options) {
+        
+        var getGalleryContWidth = function(){
+            return galleryCont.width();
+        }
+        
+        var obeyConstraints = function() {
+            for (var i=0, n=data.photos.length, photos=data.photos; i<n; i++) {
+                photos[i].thumb_fixed_width = photos[i].thumb_width;
+                photos[i].thumb_fixed_height = photos[i].thumb_height;
+                if (photos[i].thumb_width > options.maxWidth) {
+                    photos[i].thumb_fixed_height = photos[i].thumb_height * (options.maxWidth / photos[i].thumb_width);
+                    photos[i].thumb_fixed_width = options.maxWidth;
+                } else if (photos[i].thumb_height > options.maxHeight) {
+                    photos[i].thumb_fixed_width = photos[i].thumb_width * (options.maxHeight / photos[i].thumb_height);
+                    photos[i].thumb_fixed_height = options.maxHeight;
+                }
+            }
+        }
+        
+        var splitByRows = function(){
+            var curImagesWidth = 0;
+            var firstInRow = 0;
+            var j, k;
+            for (var i=0, n=data.photos.length, photos=data.photos; i<n; i++) {
+                photos[i].thumb_inrow_width = photos[i].thumb_fixed_width;
+                photos[i].thumb_inrow_height = photos[i].thumb_fixed_height;
+                curImagesWidth += photos[i].thumb_fixed_width + options.interval;
+                if (curImagesWidth >= galleryContWidth) {
+                    k = (curImagesWidth - (i-firstInRow+1)*options.interval) / (galleryContWidth - (i-firstInRow+1)*options.interval);
+                    for (j=firstInRow; j<=i; j++) {
+                        photos[j].thumb_inrow_width = Math.floor(photos[j].thumb_fixed_width / k);
+                        photos[j].thumb_inrow_height = Math.floor(photos[j].thumb_fixed_height / k);
+                    }
+                    curImagesWidth = 0;
+                    firstInRow = i + 1;
+                }
+            }
+        }
+        
+        var createImages = function(){
+            var image, link;
+            for (var i=0, n=data.photos.length, photos=data.photos; i<n; i++) {
+                image = $('<img/>');
+                link = $('<a/>');
+                image.attr({
+                    src: photos[i].src,
+                    width: photos[i].thumb_inrow_width,
+                    height: photos[i].thumb_inrow_height,
+                    'class': 'b-gallery__item'
+                });
+                link.append(image);
+                galleryCont.append(link);
+            }
+        }
+        
+        var setInrowSizes = function(){
+            galleryCont.find('img').each(function(i){
+                $(this).attr({
+                    width: data.photos[i].thumb_inrow_width,
+                    height: data.photos[i].thumb_inrow_height
+                });
+            });
+        }
+        
+        $(window).resize(function(){
+            galleryContWidth = getGalleryContWidth();
+            splitByRows();
+            setInrowSizes();
+        });
+        
+        var galleryCont = this;
+        var galleryContWidth = getGalleryContWidth();
+        var data = options.data;
+        obeyConstraints();
+        splitByRows();
+        createImages();
+    };
+})( jQuery );
